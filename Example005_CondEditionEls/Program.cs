d5a5260c813f803bec4b9fbe26caecde5f56e330
egor-turaev <egor-turaev@mail.ru>
2022-08-04 17:25:37 +0300

    005

---
+++ Example005_CondEditionEls/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользовотеля: ");
+string useranme = Console.ReadLine();
+
+if(useranme.ToLower() == "маша")
+{
+     Console.WriteLine("УРА, это же МАША!");
+}
+else 
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(useranme);
+}
